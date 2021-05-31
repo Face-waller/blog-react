f9cb140df80385877091e6154b44bc5e68028252
lulin <2445530702@qq.com>
2021-05-31 18:14:21 +0800

    留言模块

--- src/components/comment-leave-item.jsx
+++ src/components/comment-leave-item.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import {withRouter} from "react-router";
 
-function CommentLeaveItem(props) {
+function CommentLeaveItem({msgItem}) {
+    console.log(`msgItem.msgUser.headerUrl : ==> ${msgItem.msgUser.headerUrl}`)
     return (
-        <div>comment</div>
+        <li className="item cl">
+            <a href="#">
+                <i className="avatar size-L radius">
+                    <img alt="" src={msgItem.msgUser.headerUrl}/>
+                </i>
+            </a>
+            <div className="comment-main">
+                <header className="comment-header">
+                    <div className="comment-meta"><a className="comment-author" href="#">{msgItem.msgUser.name}</a>
+                        <div className="f-r">2014-8-31
+                            {msgItem.time}
+                        </div>
+                    </div>
+                </header>
+                <div className="comment-body">
+                    {msgItem.details}
+                    <ul className="commentList">
+                        {
+                            msgItem.reply.length > 0 ?
+                            msgItem.reply.map(n=> <CommentLeaveItem key={n.id} msgItem={n} />) :
+                            null
+                        }
+                    </ul>
+                    <button className="hf f-r btn btn-default size-S mt-10" name="2">回复</button>
+                </div>
+            </div>
+        </li>
     )
 }
 
--- src/pages/board/board.jsx
+++ src/pages/board/board.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useRef} from "react";
 import {withRouter} from "react-router";
 import 'src/pages/board/board.scss'
 import ReactWEditor from 'wangeditor-for-react';
+import CommentLeaveItem from "src/components/comment-leave-item";
 
 function Board(props) {
     const editorConfig = {
@@ -17,6 +18,44 @@ function Board(props) {
             }
         ]
     }
+
+    const msgList = [
+        {
+            id: 1,
+            pid: 0,
+            msgUser: {
+                name: '老王',
+                headerUrl: 'http://q.qlogo.cn/qqapp/101388738/1CF8425D24660DB8C3EBB76C03D95F35/100'
+            },
+            time: '2014-8-31 8:30',
+            details: '你是猴子派来的救兵吗',
+            reply: [
+                {
+                    id: 1,
+                    pid: 1,
+                    msgUser: {
+                        name: '小李',
+                        headerUrl: 'http://q.qlogo.cn/qqapp/101388738/1CF8425D24660DB8C3EBB76C03D95F35/100'
+                    },
+                    time: '2014-8-31 8:35',
+                    details: '显然不是',
+                    reply: [],
+                }
+            ]
+        },
+        {
+            id: 2,
+            pid: 0,
+            msgUser: {
+                name: '小王',
+                headerUrl: 'http://qzapp.qlogo.cn/qzapp/101388738/1CF8425D24660DB8C3EBB76C03D95F35/100'
+            },
+            time: '2014-8-31 9:30',
+            details: '你是猴子派来的逗比吗',
+            reply: []
+        },
+
+    ]
 
     return (
         <div className="col-xs-12 col-md-10 col-md-offset-1">
@@ -37,93 +76,10 @@ function Board(props) {
             </div>
             <div className="line"/>
             <ul className="commentList mt-50">
-                <li className="item cl">
-                    <a href="#">
-                        <i className="avatar size-L radius">
-                            <img alt="" src="http://q.qlogo.cn/qqapp/101388738/1CF8425D24660DB8C3EBB76C03D95F35/100"/>
-                        </i>
-                    </a>
-                    <div className="comment-main">
-                        <header className="comment-header">
-                            <div className="comment-meta"><a className="comment-author" href="#">老王</a>
-                                <time title="2014年8月31日 下午3:20" dateTime="2014-08-31T03:54:20" className="f-r">2014-8-31
-                                    15:20
-                                </time>
-                            </div>
-                        </header>
-                        <div className="comment-body">
-                            你是猴子派来的救兵吗？
-
-                            <ul className="commentList">
-                                <li className="item cl">
-                                    <a href="#">
-                                        <i className="avatar size-L radius">
-                                            <img alt="" src="http://qzapp.qlogo.cn/qzapp/101388738/1CF8425D24660DB8C3EBB76C03D95F35/100"/>
-                                        </i>
-                                    </a>
-                                    <div className="comment-main">
-                                        <header className="comment-header">
-                                            <div className="comment-meta"><a className="comment-author" href="#">老王</a>
-                                                <time title="2014年8月31日 下午3:20" dateTime="2014-08-31T03:54:20"
-                                                      className="f-r">2014-8-31 15:20
-                                                </time>
-                                            </div>
-                                        </header>
-                                        <div className="comment-body">
-                                            <p> 是的</p>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="item cl"><a href="#"><i className="avatar size-L radius"><img alt=""
-                                                                                                             src="http://qzapp.qlogo.cn/qzapp/101388738/696C8A17B3383B88804BA92ECBAA5D81/100"/></i></a>
-                                    <div className="comment-main">
-                                        <header className="comment-header">
-                                            <div className="comment-meta"><a className="comment-author" href="#">老王</a>
-                                                <time title="2014年8月31日 下午3:20" dateTime="2014-08-31T03:54:20"
-                                                      className="f-r">2014-8-31 15:20
-                                                </time>
-                                            </div>
-                                        </header>
-                                        <div className="comment-body">
-                                            <p> +1</p>
-                                        </div>
-                                    </div>
-                                </li>
-
-                            </ul>
-
-                            <button className="hf f-r btn btn-default size-S mt-10" name="2">回复</button>
-
-                        </div>
-                    </div>
-                </li>
-                <li className="item cl"><a href="#"><i className="avatar size-L radius"><img alt=""
-                                                                                             src="http://qzapp.qlogo.cn/qzapp/101388738/1CF8425D24660DB8C3EBB76C03D95F35/100"/></i></a>
-                    <div className="comment-main">
-                        <header className="comment-header">
-                            <div className="comment-meta"><a className="comment-author" href="#">老王</a>
-                                <time title="2014年8月31日 下午3:20" dateTime="2014-08-31T03:54:20" className="f-r">2014-8-31
-                                    15:20
-                                </time>
-                            </div>
-                        </header>
-                        <div className="comment-body">
-                            你是猴子派来的救兵吗？
-
-                            <button className="hf f-r btn btn-default size-S mt-10" name="3">回复</button>
-
-                        </div>
-                    </div>
-                </li>
+                {
+                    msgList.map(n=><CommentLeaveItem key={n.id} msgItem={n}/>)
+                }
             </ul>
-            {/*用于回复*/}
-            <div className="comment hidden mt-20">
-                <div id="err2" className="Huialert Huialert-danger hidden radius">成功状态提示</div>
-                <textarea className="textarea" style={{height: 100}}> </textarea>
-                <button type="button" className="btn btn-primary radius mt-10">回复</button>
-                <a className="cancelReply f-r mt-10">取消回复</a>
-            </div>
-
         </div>
     )
 }
