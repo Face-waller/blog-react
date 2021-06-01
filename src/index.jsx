d2da7d18730ebbb8426d30e483cf936b7db514f5
lulin <2445530702@qq.com>
2021-06-01 16:57:38 +0800

    1

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import 'src/static/css/common.less'
 import {BrowserRouter} from "react-router-dom";
 import {Route} from "react-router";
 
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
